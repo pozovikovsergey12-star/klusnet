e69a568f846db74a4cfe58b754b249940455bbb5
Sergey <sergey@MacBook-Air-Sergey.local>
2026-05-20 15:14:06 +0200

    fix homepage clean

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,16 @@
 export default function Home() {
   return (
-    <main style={{
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      height: "100vh",
-      fontSize: "40px",
-      fontWeight: "bold"
-    }}>
+    <main
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        fontSize: "40px",
+        fontWeight: "bold",
+      }}
+    >
       KlusNet работает 🚀
     </main>
-  );
-}
-
-          </a>
-        </div>
-      </main>
-    </div>
   );
 }
